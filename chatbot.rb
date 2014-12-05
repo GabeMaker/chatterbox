b56609862dbaa165d42d3f97b836b6e92ef0a1f1
gabe <GabeMaker@users.noreply.github.com>
2014-12-05 12:04:43 +0000

    added ability to handle 3 regular expression capture groups (and very contrived example)

--- chatbot.rb
+++ chatbot.rb
@@ -2,7 +2,7 @@ def get_response(input)
   key = RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample
   /#{key}/ =~ input
   response = RESPONSES[key]
-  response.nil? ? 'sorry?' : response % { c1: $1, c2: $2}
+  response.nil? ? 'sorry?' : response % { c1: $1, c2: $2, c3: $3}
 end
 
 # courtesy of http://stackoverflow.com/questions/1489183/colorized-ruby-output
@@ -11,7 +11,9 @@ def red;            "\033[31m#{self}\033[0m" end
 def green;          "\033[32m#{self}\033[0m" end
 end
 
-RESPONSES = { 'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
+RESPONSES = { 'A=(.*) B=(.*) C=(.*)' => 'Then %{c1} squared equals %{c2} squared plus %{c3} squared - assuming A is the length of the longest side of a right angled triangle and B and C are the remaining lengths. Simple, eh?',
+
+			  'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
               'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
           	  'I hate (.*)'=> 'I don\'t hate %{c1}, but I\'m not that keen',
