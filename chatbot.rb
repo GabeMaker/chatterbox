075b47e7dd2804945e094bd3382eb5ebf6a9e88a
gabe <GabeMaker@users.noreply.github.com>
2014-12-05 09:07:43 +0000

    added 12 responses (to what I wanted to say after first try

--- chatbot.rb
+++ chatbot.rb
@@ -9,7 +9,19 @@ RESPONSES = { 'goodbye' => 'bye',
               'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
-              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}'}
+              'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
+          	  'Hi' => 'Hi there!',
+          	  'Hello' => 'Hello!',
+          	  'hi' => 'hi!',
+          	  'hello' => 'hello!',
+          	  'how are you?' => 'Pretty good, thank you. And you?',
+          	  'ok' => 'OK is okay... Could be worse I guess',
+          	  'good' => 'Good? good is great!',
+          	  'yeah' => 'yep',
+          	  'name' => 'My name is Chatbot',
+          	  'chatbot' => 'That\'s my name!',
+          	  'Chatbot' => 'That\'s what they call me!',
+          	  'help' => 'If you feel stuck with me (and you\'re running me from command line using "ruby chatbot.rb") press ctrl + C to exit'}
 
 puts "Hello, what's your name?"
 name = gets.chomp
